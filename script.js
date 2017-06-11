03f91abe57059a5356eb8fe1241e11fd70bb6188
Oleksandr Blyznuk <blyzniuk.oleksandr@gmail.com>
2017-06-11 21:59:23 +0300

    Optimized styles

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-var timer = document.getElementById('timer');
-var day = 1000*60*60*24;
-var hour = 1000*60*60;
-var minute = 1000*60;
-var second = 1000;
-var diffTime = new Date(2017, 5, 26, 11).getTime() - new Date().getTime();
+var timer = document.getElementById('timer'),
+    day = 1000*60*60*24,
+    hour = 1000*60*60,
+    minute = 1000*60,
+    second = 1000,
+    diffTime = new Date(2017, 5, 26, 11).getTime() - new Date().getTime();
 
 function getWithZero(number) {
-    return number > 9 ? number : `0${number}`;
-};
+    return number > 9 ? number : '0' + number;
+}
 
-setInterval(() => {
-    var days = Math.floor(diffTime/day),
-    timeLeft = diffTime - days*day,
-    hours = Math.floor(timeLeft/hour),
-    timeLeft = timeLeft - hours*hour,    
-    minutes = Math.floor(timeLeft/minute),
-    timeLeft = timeLeft - minutes*minute,    
+setInterval(function() {
+    var days = Math.floor(diffTime/day), timeLeft = diffTime - days*day,
+    hours = Math.floor(timeLeft/hour), timeLeft = timeLeft - hours*hour,
+    minutes = Math.floor(timeLeft/minute), timeLeft = timeLeft - minutes*minute,
     seconds = Math.floor(timeLeft/second),
     time = [days, getWithZero(hours), getWithZero(minutes), getWithZero(seconds)];
 
-    timer.innerText = time.join('  :  ');;
+    timer.innerText = time.join('  :  ');
     diffTime -= second;
 }, second);
-
