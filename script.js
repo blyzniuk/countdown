5eebc2249b263fe17d74607a84674d8b05c2da9c
Oleksandr Blyznuk <blyzniuk.oleksandr@gmail.com>
2017-06-20 09:13:17 +0300

    Added service worker

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ setInterval(function() {
     timer.innerText = time.join('  :  ');
     diffTime -= second;
 }, second);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./sw.js', { scope: './' })
+        .then(function(registration) {
+            console.log('Service Worker Registered', registration);
+        })
+        .catch(function(err) {
+            console.log('Service Worker Registration Failed', err);
+        });
+}
